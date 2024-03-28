aec3026ffc8f64c2d006756a5beca47266971538
Muzammel Osmani Azizi <ms7.swaggy@gmail.com>
2024-03-28 06:53:02 -0700

    done

--- app.js
+++ app.js
@@ -4,7 +4,29 @@ console.log(
 );
 
 // Exercise 1
-
+let lastName; 
+var age
 // Exercise 2
+const firstName = "Muzammel";
+lastName = "Osmani";
+var age = 25;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSenitive = true;
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salesPercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
